baa0aa0760badd4975aeb8db899f48469b7fc221
Ahmad200ali <1116960@hr.nl>
2026-09-11 13:49:01 +0200

    Quest basic

--- Quest.cs
+++ Quest.cs
@@ -1,4 +1,16 @@
 public class Quest
 {
+    public int ID;
+    public string Name;
+    public string Description;
+
+    public Quest(int id, string name,string description)
+    {
+        ID = id;
+        Name = name;
+        Description = description;
+    }
+
+
     
 }
