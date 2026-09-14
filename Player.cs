49341b4349b0bd5e17fc48f125934172eb647f9b
nebi <122598443+romanized@users.noreply.github.com>
2026-09-14 23:18:29 +0200

    player health

--- Player.cs
+++ Player.cs
@@ -1,4 +1,42 @@
 public class Player
 {
-    
+    public string Name;
+    public int CurrentHitPoints;
+    public int MaximumHitPoints;
+
+    public Player(string name, int maximumHitPoints)
+    {
+        Name = name;
+        MaximumHitPoints = maximumHitPoints;
+        CurrentHitPoints = maximumHitPoints;
+    }
+
+    public void ShowHealth()
+    {
+        Console.WriteLine($"Health: {CurrentHitPoints}/{MaximumHitPoints}");
+    }
+
+    public void TakeDamage(int damage)
+    {
+        CurrentHitPoints -= damage;
+        if (CurrentHitPoints < 0)
+        {
+            CurrentHitPoints = 0;
+        }
+
+        Console.WriteLine($"You lost {damage} health.");
+        ShowHealth();
+    }
+
+    public void Heal(int amount)
+    {
+        CurrentHitPoints += amount;
+        if (CurrentHitPoints > MaximumHitPoints)
+        {
+            CurrentHitPoints = MaximumHitPoints;
+        }
+
+        Console.WriteLine($"You recovered {amount} health.");
+        ShowHealth();
+    }
 }
